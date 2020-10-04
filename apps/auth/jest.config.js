eeb539342b614a53719b3041040e36714db3158e
karoldavid <k.zyskowski@live.com>
2020-10-04 16:08:52 +0200

    initial setup

---
+++ apps/auth/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'auth',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  transform: {
+    '^.+\\.[tj]s$':  'ts-jest' 
+  },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/auth'
+};
