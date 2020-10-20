00cb505289771bda1c5ad2a92b2fc81873adfce5
karoldavid <k.zyskowski@live.com>
2020-10-20 21:16:08 +0200

    feat: add aut0 and getEvents handler

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
-var nodeExternals = require('webpack-node-externals')
+// var nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  externals: [nodeExternals()],
+  // externals: [nodeExternals()],
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
