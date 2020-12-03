a8c3c528a91781af93c0573d48c881078f4d0e24
karoldavid <k.zyskowski@live.com>
2020-12-03 20:06:20 +0100

    feat: use a template for aws ses attendance confirmation emails

--- apps/attendance/src/app/lambda/dynamodb/onAttendeeSignup.ts
+++ apps/attendance/src/app/lambda/dynamodb/onAttendeeSignup.ts
@@ -9,16 +9,19 @@ const logger = createLogger('onAttendeeSignup');
 export const handler: DynamoDBStreamHandler = async (
   event: DynamoDBStreamEvent
 ) => {
-  logger.info(`Processing events batch from DynamoDB ${JSON.stringify(event)}`);
-
   for (const record of event.Records) {
     logger.info(`Processing record ${JSON.stringify(record)}`);
     if (record.eventName === 'INSERT') {
-      const response = await sendConfirmationEmail(
-        record.dynamodb.NewImage.email.S,
-        record.dynamodb.NewImage.name.S
-      );
-      logger.info(response);
+      try {
+        const response = await sendConfirmationEmail(
+          record.dynamodb.NewImage.email.S,
+          process.env.sesSenderEmail,
+          record.dynamodb.NewImage.name.S
+        );
+        logger.info(response);
+      } catch (e) {
+        logger.info(e);
+      }
     }
   }
 };
--- apps/attendance/src/app/ses/confirmationEmail.ts
+++ apps/attendance/src/app/ses/confirmationEmail.ts
@@ -1,33 +1,45 @@
 import * as AWS from 'aws-sdk';
-import { SendEmailResponse } from 'aws-sdk/clients/ses';
+import { SendTemplatedEmailResponse } from 'aws-sdk/clients/ses';
 
 import { createLogger } from '../utils/logger';
+
+const templateParams = require('./confirmation-email-template.json');
 
 const logger = createLogger('sendConfirmationEmail');
 
 const SES = new AWS.SES({ region: process.env.AWS_REGION });
 
-export const sendConfirmationEmail = (
+const { TemplateName: templateName } = templateParams.Template;
+
+export const sendConfirmationEmail = async (
   recipient: string,
+  sender: string,
   name: string
-): Promise<SendEmailResponse> => {
+): Promise<SendTemplatedEmailResponse> => {
+  try {
+    logger.info(`Checking if template ${templateName} exists.`);
+    await SES.getTemplate({ TemplateName: templateName }).promise();
+    logger.info(`Template ${templateName} exists.`);
+  } catch (e) {
+    logger.info(e);
+    logger.info(`Creating template ${templateName}.`);
+    await SES.createTemplate(templateParams).promise();
+  }
+
+  const templateData = JSON.stringify({
+    name,
+  });
+
   const params = {
     Destination: {
       ToAddresses: [recipient],
     },
-    Message: {
-      Body: {
-        Text: {
-          Data: `Dear ${name},\n\n Thank you very much for registering your attendance.\n\n Best Regards`,
-        },
-      },
-
-      Subject: { Data: 'Signup Successfull' },
-    },
-    Source: process.env.SES_SENDER_EMAIL,
+    Source: sender,
+    Template: templateName,
+    TemplateData: templateData,
   };
 
   logger.info(`Sending confirmation email to: ${recipient}`);
 
-  return SES.sendEmail(params).promise();
+  return SES.sendTemplatedEmail(params).promise();
 };
