c65e620e9ee593d9285a85800fde52be6f847634
karoldavid <k.zyskowski@live.com>
2020-12-24 19:44:47 +0100

    chore: enable aws x-ray distributed tracing

--- apps/attendance/src/app/dataLayer/dynamoDBClient.ts
+++ apps/attendance/src/app/dataLayer/dynamoDBClient.ts
@@ -1,16 +1,19 @@
 import * as AWS from 'aws-sdk';
+import * as AWSXRay from 'aws-xray-sdk';
 import { createLogger } from '../utils/logger';
 
 const logger = createLogger('dynamoDBClient');
 
+const XAWS = AWSXRay.captureAWS(AWS);
+
 export function createDynamoDBClient() {
   if (process.env.IS_OFFLINE) {
     logger.info('Creating a local DynamoDB instance');
-    return new AWS.DynamoDB.DocumentClient({
+    return new XAWS.DynamoDB.DocumentClient({
       region: 'localhost',
       endpoint: 'http://localhost:8002',
     });
   }
 
-  return new AWS.DynamoDB.DocumentClient();
+  return new XAWS.DynamoDB.DocumentClient();
 }
